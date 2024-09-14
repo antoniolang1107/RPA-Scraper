89e095d37b1c9166dcb54a08e2de4cb444a0dbcf
antoniolang1107 <TonylsLame@users.noreply.github.com>
2024-09-14 12:55:24 -0700

    Preliminary implementation to run job and send results

---
+++ conductor.py
@@ -0,0 +1,18 @@
+"""Module to query for and send emails"""
+
+import os
+
+from dotenv import load_dotenv
+from email_service import gmail_authenticate, send_message, generate_message_html
+from scraper import run_job, read_config
+
+load_dotenv()
+receiver_email: str = os.environ["RECEIVER_ADDRESS"]
+job_config_fname: str = os.environ["CONFIG_FNAME"]
+
+if __name__ == "__main__":
+    job_results: dict = run_job(read_config(job_config_fname))
+
+    email_service = gmail_authenticate()
+    html_message: str = generate_message_html(job_results)
+    send_message(email_service, receiver_email, "Test Subject", html_message)
